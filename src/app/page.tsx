077f7a21ba8738979445803f4c22f4b2e25ef0c9
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-31 16:42:24 -0600

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+"use client"
+import { useUser } from "@clerk/nextjs";
+import Home from "./protected/page";
+import Landing from "./landing/page";
+
+export default function Page() {
+  const { isSignedIn } = useUser();
+
   return (
-    <div>I changed it</div>
+    <div>
+      {isSignedIn? <Home/>: <Landing/>}
+    </div>
   );
 }
